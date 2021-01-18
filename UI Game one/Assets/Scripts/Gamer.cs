25c70496965685c3186252829074628d54801e41
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-01-18 17:24:24 -0500

    added the last features to the game, including guessing a number and able to increase and decrease the number of guess by button.

---
+++ UI Game one/Assets/Scripts/Gamer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Gamer : MonoBehaviour
+{
+   [SerializeField] int max;
+   [SerializeField] int min;
+   [SerializeField] TextMeshProUGUI guesstext;
+    int guess;
+
+
+
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+       
+    }
+
+   
+
+    public void OnpressHigher()
+    {
+        min = guess + 1;
+        NextGuess();
+    }
+
+
+
+    public void OnpressLower()
+    {
+        max = guess - 1;
+        NextGuess();
+    }
+
+
+    void NextGuess()
+    {
+        guess = Random.Range(min,max + 1);
+        guesstext.text = guess.ToString();
+    }
+
+
+}
+
